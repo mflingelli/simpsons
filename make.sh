0cb8fe5ecc2ca0fed90151a73d44aa4132d71d8a
Markus Flingelli <markus.flingelli@gmx.de>
2018-11-21 22:29:44 +0100

    Linux Make-Script überarbeitet.

--- make.sh
+++ make.sh
@@ -5,8 +5,7 @@ bibtex "Simpsons"
 pdflatex Simpsons.tex
 pdflatex Simpsons.tex
 makeindex -g -s header.ist Simpsons.idx
-# TODO: Fix index
-del Simpsons.ind
-ren Simpsons.ind.new Simpsons.ind
 pdflatex Simpsons.tex
 pdflatex Simpsons.tex
+
+exit 0
