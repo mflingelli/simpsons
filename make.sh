9bcad5ce0212a41113dd8e23eaa7903eedc66afc
Markus Flingelli <markus.flingelli@gmx.de>
2021-08-06 13:53:58 +0200

    Unnötige Dateien nach Build-Vorgang löschen.

--- make.sh
+++ make.sh
@@ -8,4 +8,16 @@ makeindex -g -s header.ist Simpsons.idx
 pdflatex Simpsons.tex
 pdflatex Simpsons.tex
 
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.brf
+rm *.idx
+rm *.ilg
+rm *.ind
+rm *.log
+rm *.out
+rm *.gz
+rm *.toc
+
 exit 0
